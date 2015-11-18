662884cf6889bfc5521767c9befea4b75bb349d1
Nuria Gonzalo <alu0100598155@ull.edu.es>
2015-11-18 18:17:09 +0000

    LDH1-1 #Tarea onClick, enlace boton con Sensor

--- app/src/main/java/com/example/nuni/ldh1/MainActivity.java
+++ app/src/main/java/com/example/nuni/ldh1/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.nuni.ldh1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
+
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+    public void pasarActividad2(View v) {
+        Intent ace = new Intent(this, prueba1.class);
+        startActivity(ace);
+
     }
 
     @Override
@@ -49,4 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
