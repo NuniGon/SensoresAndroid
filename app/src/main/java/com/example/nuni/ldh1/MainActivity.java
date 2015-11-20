82c83a613f2bf8594bcd87f99f8d32ff1e74caa6
Nuria Gonzalo <alu0100598155@ull.edu.es>
2015-11-20 21:12:45 +0000

    LDH1-1 #Sensor #Acelerometro #done Sensor Acelerometro

--- app/src/main/java/com/example/nuni/ldh1/MainActivity.java
+++ app/src/main/java/com/example/nuni/ldh1/MainActivity.java
@@ -1,26 +1,33 @@
 package com.example.nuni.ldh1;
-
-import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
-
 import android.view.MenuItem;
+import android.widget.TextView;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    TextView texto;
+    SensorManager sensorManager;
+    private Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar); FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,15 +35,20 @@ public class MainActivity extends AppCompatActivity{
                         .setAction("Action", null).show();
             }
         });
+
+        texto = (TextView)findViewById(R.id.texto);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+        /*List<Sensor> mList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for (int i=1 ; i<mList.size() ; i++)
+            texto.append("\n" +mList.get(i).getName()+ "\n" +mList.get(i).getVendor()+ "\n" +mList.get(i).getVersion());
+        */
+
     }
-
-
-    public void pasarActividad2(View v) {
-        Intent ace = new Intent(this, prueba1.class);
-        startActivity(ace);
-
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,6 +72,33 @@ public class MainActivity extends AppCompatActivity{
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x,y, z;
+        x = event.values[0];
+        y = event.values[1];
+        z = event.values[2];
 
+        texto.setText("");
+        texto.append("\n" + " Valor de X: " + x + "\n" + " Valor de Y: " + y + "\n" + " Valor de Z: " + z);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 
 }
