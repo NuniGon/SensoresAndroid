854dba98d5989de5038b930bf43731da4102f6fc
Nuria Gonzalo <alu0100598155@ull.edu.es>
2015-11-20 12:12:38 +0000

    arreglando 2

--- app/src/main/java/com/example/nuni/ldh1/MainActivity.java
+++ app/src/main/java/com/example/nuni/ldh1/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.nuni.ldh1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
+
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        setSupportActionBar(toolbar); FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void pasarActividad2(View v) {
+        Intent ace = new Intent(this, prueba1.class);
+        startActivity(ace);
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
