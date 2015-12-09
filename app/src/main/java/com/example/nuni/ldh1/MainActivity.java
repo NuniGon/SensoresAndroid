932b523981896832d1e2805395bfdaba7d3432fb
Nuria Gonzalo <alu0100598155@ull.edu.es>
2015-12-09 18:12:39 +0000

    #LDH1-1 #Interfaz #Botones #done Interfaz creada

--- app/src/main/java/com/example/nuni/ldh1/MainActivity.java
+++ app/src/main/java/com/example/nuni/ldh1/MainActivity.java
@@ -9,7 +9,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.content.Intent;
+import android.widget.Button;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    Button botonAcelerometro;
+    Button botonGiroscopio;
+    Button botonPodometro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,48 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        /**
+         * Se declara boton para el sensor ACELEROMETRO
+         */
+        botonAcelerometro = (Button) findViewById(R.id.button);
+        botonAcelerometro.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                acelometer(v);
+            }
+        });
+
+        /**
+         * Se declara el boton para el sensor GIROSCOPIO
+         */
+
+        botonGiroscopio = (Button) findViewById(R.id.button2);
+        botonGiroscopio.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                giroscopio(v);
+            }
+        });
+
+        /**
+         * Se declara boton para el sensor PODOMETRO
+         */
+        botonPodometro = (Button) findViewById(R.id.button3);
+        botonPodometro.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                podometro(v);
+            }
+        });
+
     }
 
+    /**
+     * Añade elementos a la barra de acción si está presente
+     * @param menu
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -35,6 +84,11 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Coge el id , comprueba si está en los ajustes, si está return true, si no, retorna el item seleccionado en las opciones
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -48,5 +102,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Función que crea el intent con la actividad del sensor proximidad y la inicia.
+     * Se espera modificar cambios (Probando commits con jira...)
+     * @param view
+     */
+    public void acelometer(View view) {
+        Intent i = new Intent(this, Acelerometro.class);
+        startActivity(i);
+    }
+
+    /**
+     * Función que crea el intent con la actividad del sensor magnometro y la inicia.
+     * @param view
+     */
+    public void giroscopio(View view) {
+        Intent i = new Intent(this, Giroscopio.class);
+        startActivity(i);
+    }
+
+    public void podometro(View view) {
+        Intent i = new Intent(this, Podometro.class);
+        startActivity(i);
     }
 }
