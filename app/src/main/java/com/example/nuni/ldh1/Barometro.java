2b0d2c74c3464541a289079a2d13ebfc7efabfc5
Núria Gonso <alu0100598155@ull.edu.es>
2016-01-12 08:47:38 +0000

    Update Barometro.java

--- app/src/main/java/com/example/nuni/ldh1/Barometro.java
+++ app/src/main/java/com/example/nuni/ldh1/Barometro.java
@@ -18,7 +18,7 @@ public class Barometro extends AppCompatActivity implements SensorEventListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_barometro);
 
         pressView = (TextView) findViewById(R.id.pressTxt);
 
